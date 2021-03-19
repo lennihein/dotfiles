96f43f37706224e6bc0ee80251fc29fd89f4e6fd
Lenni Hein <git@lennihein.com>
2021-03-19 03:17:32 +0100

    added real dir size to ls commands

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
 # Defined in /home/lenni/.config/fish/functions/ls.fish @ line 2
 function ls --wraps='nu -c "ls"' --description 'alias ls nu -c "ls"'
-  nu -c "ls -d" $argv;
+  nu -c "ls" $argv;
 end
--- .config/fish/functions/lsla.fish
+++ .config/fish/functions/lsla.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function lsla --wraps='nu -c "ls -la"' --description 'alias lsla nu -c "ls -la"'
-  nu -c "ls -la -d" $argv;
+  nu -c "ls -la" $argv;
 end
