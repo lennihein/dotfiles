3a130f39646f8a89967cafbb9ca0b9fe759cbff2
Lenni Hein <git@lennihein.com>
2022-09-29 15:34:38 +0200

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,6 @@ eval (starship init fish)
 
 thefuck --alias | source
 
+set -x DEBUGINFOD_URLS "https://debuginfod.archlinux.org"
+
 set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
