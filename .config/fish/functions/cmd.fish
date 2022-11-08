2c2a793141c84a954202ce6998cb30f0e2a05b09
Lenni Hein <git@lennihein.com>
2022-11-08 22:23:53 +0100

    .

---
+++ .config/fish/functions/cmd.fish
@@ -0,0 +1,3 @@
+function cmd --wraps=/mnt/c/Windows/System32/cmd.exe --description 'alias cmd /mnt/c/Windows/System32/cmd.exe'
+  /mnt/c/Windows/System32/cmd.exe $argv; 
+end
--- .config/fish/functions/tree.fish
+++ .config/fish/functions/tree.fish
@@ -1,5 +1,3 @@
-# Defined in - @ line 1
-function tree --description 'alias tree git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
-	# git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s" $argv;
-    git log --all --decorate --graph --abbrev-commit --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)';
+function tree --wraps='exa -T' --description 'alias tree exa -T'
+  exa -T $argv; 
 end
