4b5da37b2f292f9a51e2ea56add1bdfaf45282ca
Lenni Hein <git@lennihein.com>
2022-11-17 15:59:50 +0000

    adjusted for ubuntu

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,5 @@
-#!/usr/bin/env fish
-
-export DISPLAY=(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
-export GDK_SCALE=2
-export QT_SCALE_FACTOR=2
-
-eval (starship init fish)
-
-thefuck --alias | source
-
-set -x DEBUGINFOD_URLS "https://debuginfod.archlinux.org"
-
-set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+source (/home/linuxbrew/.linuxbrew/Cellar/starship/1.11.0/bin/starship init fish --print-full-init | psub)
