a34f249b24fefad8d35a1ca6650074753bcf392d
Lenni Hein <git@lennihein.com>
2022-11-18 15:25:26 +0100

    set up for debian wsl

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+export DISPLAY=(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+export GDK_SCALE=2
+export QT_SCALE_FACTOR=2
+
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 source (/home/linuxbrew/.linuxbrew/Cellar/starship/1.11.0/bin/starship init fish --print-full-init | psub)
