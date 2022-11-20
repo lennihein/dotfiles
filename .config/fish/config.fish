b67a122ad626744204b74a9b646008e9e36e8e5f
Lenni Hein <git@lennihein.com>
2022-11-20 23:00:21 +0100

    switch from brew to nix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,5 +8,4 @@ export GDK_BACKEND=x11
 export GDK_SCALE=2
 export QT_SCALE_FACTOR=2
 
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-source (/home/linuxbrew/.linuxbrew/Cellar/starship/1.11.0/bin/starship init fish --print-full-init | psub)
+source (/nix/store/ivfanhg18amh1hg0pyiqwfs7s7msf22j-starship-1.11.0/bin/starship init fish --print-full-init | psub)
