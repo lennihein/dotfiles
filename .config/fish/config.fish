def00969f520570b59c007df05782f3feb713683
Lenni Hein <git@lennihein.com>
2022-11-21 22:23:33 +0100

    switch from brew to nix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-source (/home/linuxbrew/.linuxbrew/Cellar/starship/1.11.0/bin/starship init fish --print-full-init | psub)
+
+export GDK_BACKEND=x11
+export GDK_SCALE=2
+export QT_SCALE_FACTOR=2
+
+source (/nix/store/ivfanhg18amh1hg0pyiqwfs7s7msf22j-starship-1.11.0/bin/starship init fish --print-full-init | psub)
