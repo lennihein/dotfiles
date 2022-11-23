8018d8344b92230a3b961ff62d9dd9c66a6bf69a
Lenni Hein <git@lennihein.com>
2022-11-23 03:29:58 +0100

    fixed starship init from nixos

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,4 @@ export GDK_BACKEND=x11
 export GDK_SCALE=2
 export QT_SCALE_FACTOR=2
 
-source (/nix/store/ivfanhg18amh1hg0pyiqwfs7s7msf22j-starship-1.11.0/bin/starship init fish --print-full-init | psub)
+source (starship init fish --print-full-init | psub)
