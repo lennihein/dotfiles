1027d8e2b71c1228966e9c67f7bee04b770fe87e
Lenni Hein <git@lennihein.com>
2022-12-17 21:32:56 +0100

    no WSL stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-export DISPLAY=(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
-export GDK_BACKEND=x11
-
-export GDK_SCALE=2
-export QT_SCALE_FACTOR=2
-
 source (starship init fish --print-full-init | psub)
