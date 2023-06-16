830230bd241577579be1b8630a6f85461f7e71ca
Lenni Hein <git@lennihein.com>
2023-06-16 13:08:03 +0200

    fix manpager

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,4 @@ end
 
 eval (starship init fish)
 
-set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c     \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+set -x MANPAGER 'bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c     \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
