4763ea13ce0b8c343a9fe91c83cf1f781ec3a07c
Lenni Hein <git@lennihein.com>
2023-06-20 13:00:56 +0200

    formatting

--- .configuration.nix
+++ .configuration.nix
@@ -76,8 +76,8 @@
 
     # Enable the X11 windowing system.
     services.xserver.enable = true;
-
     # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.defaultSession = "gnome";
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
 
