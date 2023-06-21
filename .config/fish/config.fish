cce223e6a0df41699d640e50a46f5964567943f3
Lenni Hein <git@lennihein.com>
2023-06-21 16:13:37 +0200

    silent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,4 +6,7 @@ eval (starship init fish)
 
 set -x MANPAGER 'bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c     \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
 
+# silent direnv
+set -x DIRENV_LOG_FORMAT ""
+# enable direnv
 direnv hook fish | source
