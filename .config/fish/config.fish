531cf4c43178136019079fab58c56f9dc4e325aa
Lenni Hein <git@lennihein.com>
2023-06-25 17:03:42 +0200

    removed WSL display config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-export DISPLAY=(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
 export GDK_BACKEND=x11
 
 export GDK_SCALE=2
