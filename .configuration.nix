407d29294b402d49dc6ac32a5f3e52b2f3bd9705
Lenni Hein <git@lennihein.com>
2023-06-30 11:46:01 +0200

    comment

--- .configuration.nix
+++ .configuration.nix
@@ -71,7 +71,7 @@
         LC_TIME = "de_DE.UTF-8";
     };
 
-    services.xserver.displayManager.gdm.wayland = true;
+    # Ozone and fractional scaling
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
     services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
         [org.gnome.mutter]
