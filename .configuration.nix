9e64eaca9a84ce52756904e01bcfd818c4097986
Lenni Hein <git@lennihein.com>
2024-03-04 10:36:15 +0100

    updated warp

--- .configuration.nix
+++ .configuration.nix
@@ -20,15 +20,9 @@
             lennihein-22-11 = import (fetchTarball "https://github.com/lennihein/nixpkgs/archive/refs/heads/nixos-22.11.zip") {
                 config = config.nixpkgs.config;
             };
-            # warp-beta = import (fetchTarball "https://github.com/imadnyc/nixpkgs/archive/refs/heads/warp-terminal-initial-linux.zip") {
-            #     config = config.nixpkgs.config;  
-            # };
-            warp = import (fetchTarball "https://github.com/lennihein/nixpkgs/archive/71f6d0f5e441be3c283fb81989180f04da5049f6.zip") {
-                config = config.nixpkgs.config;  
+            lennihein = import (fetchTarball "https://github.com/lennihein/nixpkgs/archive/refs/heads/master.zip") {
+                config = config.nixpkgs.config;
             };
-            # lennihein = import (fetchTarball "https://github.com/lennihein/nixpkgs/archive/refs/heads/master.zip") {
-            #    config = config.nixpkgs.config;
-            # };
         };
     };
 
@@ -229,7 +223,7 @@
             helix starship kitty zoxide
             
             # dev tools
-            ghidra unstable.gitkraken wireshark unstable.vscode lennihein-22-11.hyper virt-manager meld warp.warp-terminal mission-center
+            ghidra unstable.gitkraken wireshark unstable.vscode lennihein-22-11.hyper virt-manager meld lennihein.warp-terminal mission-center
             
             # others
             google-chrome discord
