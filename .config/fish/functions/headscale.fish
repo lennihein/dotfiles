2fd735bb3424aef8f1a73ec6ad1b79e4a234b7c1
Lenni Hein <git@lennihein.com>
2024-08-06 10:52:00 +0200

    removed obsolete link

--- .config/fish/functions/headscale.fish
+++
@@ -1,4 +0,0 @@
-function headscale --wraps=/nix/store/hsjkxckn9wrnfhasrrfdfhhjdf80vay9-headscale-0.22.3/bin/headscale --description 'alias headscale /nix/store/hsjkxckn9wrnfhasrrfdfhhjdf80vay9-headscale-0.22.3/bin/headscale'
-  /nix/store/hsjkxckn9wrnfhasrrfdfhhjdf80vay9-headscale-0.22.3/bin/headscale $argv
-        
-end
