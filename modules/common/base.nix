6edbec2c41d691c6707485f1868b797bbe604bdc
Lenni Hein <git@lennihein.com>
2026-08-20 14:27:02 +0200

    feat(nix): add nix-community cache and warn-dirty = false to base settings

--- modules/common/base.nix
+++ modules/common/base.nix
@@ -12,12 +12,15 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     trusted-users = [ "root" "@wheel" "lenni" ];
+    warn-dirty = false;
     substituters = [
       "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
       "https://lennihein.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "lennihein.cachix.org-1:NfNMNT0Z/t/Ykp8BKtuUAsY2PbO8GmfxJZTApFRusWo="
     ];
   };
