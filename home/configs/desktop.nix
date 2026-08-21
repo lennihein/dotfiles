1d9a3cb4ea1a1b6052eab1413552a26ea80ed311
Lenni Hein <git@lennihein.com>
2026-08-21 15:52:46 +0200

    feat: wrap antigravity-ide with --no-sandbox in desktop profile

--- home/configs/desktop.nix
+++ home/configs/desktop.nix
@@ -1,9 +1,20 @@
 { pkgs, pkgsMaster, ... }:
+let
+  antigravity-ide-sandboxed = pkgs.symlinkJoin {
+    name = "antigravity-ide";
+    paths = [ pkgsMaster.antigravity-ide ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/antigravity-ide \
+        --add-flags "--no-sandbox"
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
     google-chrome
     kitty
     obsidian
-    pkgsMaster.antigravity-ide
+    antigravity-ide-sandboxed
   ];
 }
